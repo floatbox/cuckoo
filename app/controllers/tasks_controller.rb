d260a69d343a9b199faabfbe2533bb30a556e503
Andrey Arefiev <arefiev@gmail.com>
2015-01-20 22:04:13 +0300

    Tasks limit on index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   # GET /tasks
   def index
-    @tasks = Task.order('created_at DESC')
+    @tasks = Task.order('created_at DESC').limit(20)
   end
 
 end
